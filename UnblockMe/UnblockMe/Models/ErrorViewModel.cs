eb9fb9ad59051fb464deb8cf79441f4e17ce158c
Robert <udrearobert999@gmail.com>
2021-07-01 23:02:41 +0300

    Initialized the UnblockMe website

---
+++ UnblockMe/UnblockMe/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UnblockMe.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
