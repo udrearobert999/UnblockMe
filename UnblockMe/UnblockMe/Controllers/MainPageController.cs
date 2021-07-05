772826dd7b6e07f1599179656b0f8d6ab5c5374d
Robert <udrearobert999@gmail.com>
2021-07-05 16:55:42 +0300

    Deleted data folder

--- UnblockMe/UnblockMe/Controllers/MainPageController.cs
+++ UnblockMe/UnblockMe/Controllers/MainPageController.cs
@@ -7,7 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using UnblockMe.Data;
+
 using UnblockMe.Models;
 
 namespace UnblockMe.Controllers
